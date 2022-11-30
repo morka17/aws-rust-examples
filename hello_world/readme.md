5d4f9615702758dce39caf286a250ae193536970
morka17 <joshuamorka19@gmail.com>
2022-11-30 23:15:55 +0100

    cmpleted example 1 [hello aws]

---
+++ hello_world/readme.md
@@ -0,0 +1,2 @@
+## __aws-sdk-dynamodb__
+>Amazon DynamoDB is a fully managed NoSQL database service that provides fast and predictable performance with seamless scalability. DynamoDB lets you offload the administrative burdens of operating and scaling a distributed database, so that you don't have to worry about hardware provisioning, setup and configuration, replication, software patching, or cluster scaling.
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,16 +6,26 @@ use aws_sdk_dynamodb::{Client, Error};
 /// Region isn't set
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     // firstly, Get the region
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
     // Secondly, Get the AWS config
     let config = aws_config::from_env().region(region_provider).load().await;
     // Lastly Get the client 
+    let client = Client::new(&config);
 
-    let resp = client.list_tables().send().await?
+    let resp = client.list_tables().send().await?;
 
     println!("Tables:");
 
-    
+    let names = resp.table_names().unwrap_or_default();
+
+    for name in names  {
+        println!("  {}", name);
+    }
+
+    println!();
+    println!("Found {} tables", names.len());
+
+    Ok(())
 }
